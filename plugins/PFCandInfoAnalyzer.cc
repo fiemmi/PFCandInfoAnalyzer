69107235849fbc362438d942b89bb0fd30291f0b
Fabio Iemmi <fabio.iemmi@cern.ch>
2020-08-31 19:10:07 +0200

    non-running version, error while updating to PUPPIv15

--- plugins/PFCandInfoAnalyzer.cc
+++ plugins/PFCandInfoAnalyzer.cc
@@ -82,6 +82,7 @@ class PFCandInfoAnalyzer : public edm::EDAnalyzer {
   int nvtx;
   int nPUint;
   int nAK4PUPPIJets = 0;
+  int nAK4PUPPIv15Jets = 0;
   int nAK4CHSJets = 0;
   int nAK4GenJets = 0;
   int nLeptons;
@@ -91,18 +92,20 @@ class PFCandInfoAnalyzer : public edm::EDAnalyzer {
   std::vector <float> PFCandPt,PFCandPx, PFCandPy, PFCandPz, PFCandEta, PFCandAbsEta, PFCandPhi, PFCandE, PFCandpdgId, PFCandCharge, PFCandPUPPIw, PFCandHCalFrac,
   PFCandHCalFracCalib, PFCandVtxAssQual, PFCandFromPV, PFCandLostInnerHits, PFCandTrackHighPurity, PFCandDZ, PFCandDXY, PFCandDZSig, PFCandDXYSig, PFCandNormChi2,
   PFCandQuality, PFCandNumHits, PFCandNumPixelHits, PFCandPixelLayersWithMeasurement, PFCandStripLayersWithMeasurement, PFCandTrackerLayersWithMeasurement, AK4PUPPIJetPt,
-  AK4PUPPIJetEta, AK4PUPPIJetPhi, AK4PUPPIJetE, AK4PUPPIJetRawPt, AK4PUPPIJetRawE, AK4CHSJetPt, AK4CHSJetEta, AK4CHSJetPhi, AK4CHSJetE, AK4CHSJetRawPt, AK4CHSJetRawE, 
-  AK4GenJetPt, AK4GenJetEta, AK4GenJetPhi;
+  AK4PUPPIJetEta, AK4PUPPIJetPhi, AK4PUPPIJetE, AK4PUPPIJetRawPt, AK4PUPPIJetRawE, AK4PUPPIv15JetPt, AK4PUPPIv15JetEta, AK4PUPPIv15JetPhi, AK4PUPPIv15JetE, AK4PUPPIv15JetRawPt, AK4PUPPIv15JetRawE, AK4CHSJetPt, AK4CHSJetEta, AK4CHSJetPhi, AK4CHSJetE, AK4CHSJetRawPt, AK4CHSJetRawE, AK4GenJetPt, AK4GenJetEta, AK4GenJetPhi;
   
   std::vector<std::string> triggerNames_;
   TH1F* triggerNamesHisto_;
   std::vector<bool> triggerBit_;
+
+  bool isMC;
   
   edm::Service<TFileService> fs_;
   edm::EDGetTokenT<edm::TriggerResults> triggerResultsToken_;
   edm::EDGetTokenT<reco::VertexCollection> vtxToken_;
   edm::EDGetTokenT<std::vector<PileupSummaryInfo> > PUToken_;
   edm::EDGetTokenT<pat::JetCollection> PUPPIjetToken_;
+  edm::EDGetTokenT<pat::JetCollection> PUPPIv15jetToken_;
   edm::EDGetTokenT<pat::JetCollection> CHSjetToken_;
   edm::EDGetTokenT<reco::GenJetCollection> GenjetToken_;
   edm::EDGetTokenT<pat::PackedCandidateCollection> pfcandToken_;
@@ -129,6 +132,7 @@ PFCandInfoAnalyzer::PFCandInfoAnalyzer(const edm::ParameterSet& iConfig) :
   vtxToken_(consumes<reco::VertexCollection>(iConfig.getParameter<edm::InputTag>("vertices"))),
   PUToken_(consumes<std::vector<PileupSummaryInfo> >(iConfig.getParameter<edm::InputTag>("PUinfo"))),
   PUPPIjetToken_(consumes<pat::JetCollection>(iConfig.getParameter<edm::InputTag>("AK4PUPPIJets"))),
+  PUPPIv15jetToken_(consumes<pat::JetCollection>(iConfig.getParameter<edm::InputTag>("AK4PUPPIv15Jets"))),
   CHSjetToken_(consumes<pat::JetCollection>(iConfig.getParameter<edm::InputTag>("AK4CHSJets"))),
   GenjetToken_(consumes<reco::GenJetCollection>(iConfig.getParameter<edm::InputTag>("AK4GenJets"))),
   pfcandToken_(consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>("PFCands"))),
@@ -138,6 +142,9 @@ PFCandInfoAnalyzer::PFCandInfoAnalyzer(const edm::ParameterSet& iConfig) :
   PUPPIMETToken_(consumes<pat::METCollection>(iConfig.getParameter<edm::InputTag>("PUPPImissingEt")))
   
 {
+  
+  isMC = iConfig.getUntrackedParameter<bool>("runOnMC");
+  
   //now do what ever initialization is needed
   triggerNames_ = iConfig.getParameter<std::vector<std::string> > ("triggerNames");
 
@@ -161,6 +168,7 @@ PFCandInfoAnalyzer::PFCandInfoAnalyzer(const edm::ParameterSet& iConfig) :
   outTree_->Branch("nPUint",&nPUint,"nPUint/i");
   outTree_->Branch("nLeptons", &nLeptons,"nLeptons/i");
   outTree_->Branch("nAK4PUPPIJets", &nAK4PUPPIJets,"nAK4PUPPIJets/i");
+  outTree_->Branch("nAK4PUPPIv15Jets", &nAK4PUPPIv15Jets,"nAK4PUPPIv15Jets/i");
   outTree_->Branch("nAK4CHSJets", &nAK4CHSJets,"nAK4CHSJets/i");
   outTree_->Branch("nAK4GenJets", &nAK4GenJets,"nAK4GenJets/i");
   outTree_->Branch("nPFCands", &nPFCand,"nPFCand/i");
@@ -198,6 +206,12 @@ PFCandInfoAnalyzer::PFCandInfoAnalyzer(const edm::ParameterSet& iConfig) :
   outTree_->Branch("AK4PUPPIJetE", &AK4PUPPIJetE);
   outTree_->Branch("AK4PUPPIJetRawPt", &AK4PUPPIJetRawPt);
   outTree_->Branch("AK4PUPPIJetRawE", &AK4PUPPIJetRawE);
+  outTree_->Branch("AK4PUPPIv15JetPt", &AK4PUPPIv15JetPt);
+  outTree_->Branch("AK4PUPPIv15JetEta", &AK4PUPPIv15JetEta);
+  outTree_->Branch("AK4PUPPIv15JetPhi", &AK4PUPPIv15JetPhi);
+  outTree_->Branch("AK4PUPPIv15JetE", &AK4PUPPIv15JetE);
+  outTree_->Branch("AK4PUPPIv15JetRawPt", &AK4PUPPIv15JetRawPt);
+  outTree_->Branch("AK4PUPPIv15JetRawE", &AK4PUPPIv15JetRawE);
   outTree_->Branch("AK4CHSJetPt", &AK4CHSJetPt);
   outTree_->Branch("AK4CHSJetEta", &AK4CHSJetEta);
   outTree_->Branch("AK4CHSJetPhi", &AK4CHSJetPhi);
@@ -257,32 +271,34 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
 
    //-------------- Handle pileup info ----------------------------------------------------------------------------------------------------------------------
 
-  /*        
-  Handle <std::vector<PileupSummaryInfo> > PUinfo;
-  iEvent.getByToken(PUToken_, PUinfo);
+  if (isMC) {
+        
+    Handle <std::vector<PileupSummaryInfo> > PUinfo;
+    iEvent.getByToken(PUToken_, PUinfo);
   
-  if (PUinfo->empty()) {
+    if (PUinfo->empty()) {
   
-    std::cout << "PUinfo is empty!!! skipping event..." << endl;
-    return;
+      std::cout << "PUinfo is empty!!! skipping event..." << endl;
+      return;
   
     }
 
-  std::vector<PileupSummaryInfo>::const_iterator PVI;
+    std::vector<PileupSummaryInfo>::const_iterator PVI;
 
-  for(PVI = PUinfo->begin(); PVI != PUinfo->end(); ++PVI) {
+    for(PVI = PUinfo->begin(); PVI != PUinfo->end(); ++PVI) {
 
-    int BX = PVI->getBunchCrossing();
+      int BX = PVI->getBunchCrossing();
 
-    if(BX == 0) { 
+      if(BX == 0) { 
      
-      nPUint = PVI->getPU_NumInteractions();
+	nPUint = PVI->getPU_NumInteractions();
     
-    }
+      }
 
-  }
-  */
+    }
  
+  }
+  
   //-------------- Handle trigger info ----------------------------------------------------------------------------------------------------------------------
  
   Handle<edm::TriggerResults> triggerResults;
@@ -363,6 +379,9 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
   Handle<pat::JetCollection> AK4PUPPIJets;
   iEvent.getByToken(PUPPIjetToken_, AK4PUPPIJets);
   
+  Handle<pat::JetCollection> AK4PUPPIv15Jets;
+  iEvent.getByToken(PUPPIv15jetToken_, AK4PUPPIv15Jets);
+  
   /*if(AK4PUPPIJets->empty()) {
     
     std::cout << "AK4PUPPIJets is empty!!! skipping event..." << endl;
@@ -386,20 +405,6 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
   */
   //nAK4CHSJets = AK4CHSJets->size(); count only jets with pt > 20 and |eta| < 2.4
 
-   //-------------- Handle AK4 gen jets info ----------------------------------------------------------------------------------------------------------------------
-
-  //Handle<reco::GenJetCollection> AK4GenJets;
-  //iEvent.getByToken(GenjetToken_, AK4GenJets);
-  
-  /*if(AK4GenJets->empty()) {
-    
-    std::cout << "AK4GenJets is empty!!! skipping event..." << endl;
-    return;
-  
-    }
-  */
-  //nAK4GenJets = AK4GenJets->size(); count only jets with pt > 20 and |eta| < 2.4
-  
  //-------------- Handle particle-flow candidates info ----------------------------------------------------------------------------------------------------------------------
 
   Handle<pat::PackedCandidateCollection> PFCands;
@@ -483,7 +488,6 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
   }
 
   
-
   //loop on PUPPI jets to store their Pt, Eta, Phi
   for (long unsigned int i = 0; i < AK4PUPPIJets->size(); i++) {
 
@@ -501,6 +505,22 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
 
   }
 
+  //loop on PUPPIv15 jets to store their Pt, Eta, Phi
+  for (long unsigned int i = 0; i < AK4PUPPIv15Jets->size(); i++) {
+
+    if ( AK4PUPPIv15Jets->at(i).pt() > 20 && fabs(AK4PUPPIv15Jets->at(i).eta()) < 2.4 ) {
+      
+      nAK4PUPPIv15Jets++;
+      AK4PUPPIv15JetPt.push_back(AK4PUPPIv15Jets->at(i).pt());
+      AK4PUPPIv15JetEta.push_back(AK4PUPPIv15Jets->at(i).eta());
+      AK4PUPPIv15JetPhi.push_back(AK4PUPPIv15Jets->at(i).phi());
+      AK4PUPPIv15JetE.push_back(AK4PUPPIv15Jets->at(i).energy());
+      AK4PUPPIv15JetRawPt.push_back(AK4PUPPIv15Jets->at(i).correctedJet("Uncorrected").pt());
+      AK4PUPPIv15JetRawE.push_back(AK4PUPPIv15Jets->at(i).correctedJet("Uncorrected").energy());
+      
+    }
+
+  }
    
   //loop on CHS jets to store their Pt, Eta, Phi
   for (long unsigned int i = 0; i < AK4CHSJets->size(); i++) {
@@ -519,21 +539,38 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
    
   }
 
-  /*      
-  //loop on Gen jets to store their Pt, Eta, Phi
-   for (long unsigned int i = 0; i < AK4GenJets->size(); i++) {
+  //-------------- Handle AK4 gen jets info ----------------------------------------------------------------------------------------------------------------------
 
-    if ( AK4GenJets->at(i).pt() > 20 && fabs(AK4GenJets->at(i).eta()) < 2.4 ) {
-
-      nAK4GenJets++;
-      AK4GenJetPt.push_back(AK4GenJets->at(i).pt());
-      AK4GenJetEta.push_back(AK4GenJets->at(i).eta());
-      AK4GenJetPhi.push_back(AK4GenJets->at(i).phi());
-
+  if (isMC) {
+    
+    Handle<reco::GenJetCollection> AK4GenJets;
+    iEvent.getByToken(GenjetToken_, AK4GenJets);
+    /*
+    if(AK4GenJets->empty()) {
+    
+      std::cout << "AK4GenJets is empty!!! skipping event..." << endl;
+      return;
+  
     }
+  
+    nAK4GenJets = AK4GenJets->size(); count only jets with pt > 20 and |eta| < 2.4
+    */
+      
+    //loop on Gen jets to store their Pt, Eta, Phi
+    for (long unsigned int i = 0; i < AK4GenJets->size(); i++) {
+
+      if ( AK4GenJets->at(i).pt() > 20 && fabs(AK4GenJets->at(i).eta()) < 2.4 ) {
+
+	nAK4GenJets++;
+	AK4GenJetPt.push_back(AK4GenJets->at(i).pt());
+	AK4GenJetEta.push_back(AK4GenJets->at(i).eta());
+	AK4GenJetPhi.push_back(AK4GenJets->at(i).phi());
+
+      }
    
+    }
+  
   }
-  */
   
   Handle<pat::METCollection> MET;
   iEvent.getByToken(METToken_, MET);
@@ -589,6 +626,12 @@ PFCandInfoAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSe
   AK4PUPPIJetE.clear();
   AK4PUPPIJetRawPt.clear();
   AK4PUPPIJetRawE.clear();
+  AK4PUPPIv15JetPt.clear();
+  AK4PUPPIv15JetEta.clear();
+  AK4PUPPIv15JetPhi.clear();
+  AK4PUPPIv15JetE.clear();
+  AK4PUPPIv15JetRawPt.clear();
+  AK4PUPPIv15JetRawE.clear();
   AK4CHSJetPt.clear();
   AK4CHSJetEta.clear();
   AK4CHSJetPhi.clear();
--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -5,15 +5,17 @@ process = cms.Process("PFCandInfo")
 # initialize MessageLogger and output report
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+isMC = True
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
-    #fileNames = cms.untracked.vstring('file:root://xrootd-cms.infn.it///store/mc/RunIISummer19UL17MiniAOD/QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8/MINIAODSIM/FlatPU0to70_106X_mc2017_realistic_v6-v3/40000/FFEE2656-43CF-4247-8A06-8B04E8FF00F5.root'),
+    fileNames = cms.untracked.vstring('file:root://xrootd-cms.infn.it///store/mc/RunIISummer19UL17MiniAOD/QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8/MINIAODSIM/FlatPU0to70_106X_mc2017_realistic_v6-v3/40000/FFEE2656-43CF-4247-8A06-8B04E8FF00F5.root'),
     #fileNames = cms.untracked.vstring('file:root://xrootd-cms.infn.it///store/mc/RunIISummer19UL17MiniAOD/QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8/MINIAODSIM/NoPU_106X_mc2017_realistic_v6-v3/40000/FDBFC4B3-F359-5B49-8F79-2075C6024381.root'),
     #fileNames = cms.untracked.vstring('file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_1.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_2.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_3.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_4.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_5.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_6.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_7.root', 'file:/eos/user/f/fiemmi/JetMET/ntuplize/CMSSW_10_6_4/src/pickevents_PU_files_EXT40k/pickevents_8.root',),
     #fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_1.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_2.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_3.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_4.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_5.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_6.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_7.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_8.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_9.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_10.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_11.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_12.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_13.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_14.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_15.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_16.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_17.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_18.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_19.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_20.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_21.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_22.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_23.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_24.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_25.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_26.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_27.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_28.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_29.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_30.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_31.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_32.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_33.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_34.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_35.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_36.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_37.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_38.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_39.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_40.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_41.root', 'file:/afs/cern.ch/user/f/fiemmi/JetMET/CMSSW_10_6_4/src/pickevents_PU_files_EXT_bis/pickevents_42.root',),
-    fileNames = cms.untracked.vstring('file:root://xrootd-cms.infn.it///store/data/Run2017B/JetHT/MINIAOD/09Aug2019_UL2017-v1/270000/FC1A877A-9874-D143-B7D8-E16F1F1E2BB1.root'),
+    #fileNames = cms.untracked.vstring('file:root://xrootd-cms.infn.it///store/data/Run2017B/JetHT/MINIAOD/09Aug2019_UL2017-v1/270000/FC1A877A-9874-D143-B7D8-E16F1F1E2BB1.root'),
 
     
 )
@@ -25,11 +27,27 @@ process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
                                        throw = cms.bool(False)
                                        )
 
+"""
+Update PUPPI to v15
+"""
+
+from RecoJets.JetProducers.ak4PFJets_cfi import ak4PFJets
+process.ak4PuppiJets  = ak4PFJets.clone (src = 'puppi', doAreaFastjet = True, jetPtMin = 2.)
+
+from PhysicsTools.PatAlgos.tools.jetTools import addJetCollection
+addJetCollection(process, labelName = 'Puppi', jetSource = cms.InputTag('ak4PuppiJets'), algo = 'AK', rParam=0.4, genJetCollection=cms.InputTag('slimmedGenJets'), jetCorrections = ('AK4PFPuppi', ['L1FastJet', 'L2Relative', 'L3Absolute','L2L3Residual'], 'None'), pfCandidates = cms.InputTag('packedPFCandidates'), pvSource = cms.InputTag('offlineSlimmedPrimaryVertices'), svSource = cms.InputTag('slimmedSecondaryVertices'), muSource =cms.InputTag( 'slimmedMuons'), elSource = cms.InputTag('slimmedElectrons'), genParticles= cms.InputTag('prunedGenParticles'), getJetMCFlavour=isMC)                                                                                                        
+
+process.patJetsPuppi.addGenPartonMatch = cms.bool(isMC)                                                                                                          
+process.patJetsPuppi.addGenJetMatch = cms.bool(isMC)
+
+from CommonTools.PileupAlgos.customizePuppiTune_cff import UpdatePuppiTuneV15
+UpdatePuppiTuneV15(process, isMC)
 
 process.GetPFInfo = cms.EDAnalyzer('PFCandInfoAnalyzer',
                                    vertices  = cms.InputTag("offlineSlimmedPrimaryVertices"),
                                    PUinfo  = cms.InputTag("slimmedAddPileupInfo"),
                                    AK4PUPPIJets  = cms.InputTag("slimmedJetsPuppi"),
+                                   AK4PUPPIv15Jets  = cms.InputTag("patJetsPuppi"),
                                    AK4CHSJets  = cms.InputTag("slimmedJets"),
                                    AK4GenJets = cms.InputTag("slimmedGenJets"),
                                    PFCands  = cms.InputTag("packedPFCandidates"),
@@ -43,21 +61,18 @@ process.GetPFInfo = cms.EDAnalyzer('PFCandInfoAnalyzer',
                                        "HLT_PFJet550_v",
                                    ),
                                    triggerResults = cms.InputTag('TriggerResults','','HLT'),
+                                   runOnMC = cms.untracked.bool(isMC)
 )
-
-#process.mySequence = cms.Sequence(
-#
-#    process.triggerSelection + process.GetPFInfo
-
-#)
 
 process.TFileService = cms.Service("TFileService",
                                        #fileName = cms.string('flatTree_QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8_ext.root')
                                        #fileName = cms.string('flatTree_QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8_PU_EXT.root')
                                        #fileName = cms.string('flatTree_QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8_training_noPU_EXT40k.root')
                                        #fileName = cms.string('flatTree_QCD_Pt-15to7000_TuneCP5_Flat2018_13TeV_pythia8_training_PU_EXT40k.root')
-                                       fileName = cms.string('flatTree_JetHT_Run2017B_HLTPFHT1050.root')
-                                       #fileName = cms.string('try.root')
+                                       #fileName = cms.string('flatTree_JetHT_Run2017B_HLTPFHT1050.root')
+                                       fileName = cms.string('try.root')
                                    )
 
-process.p = cms.Path(process.triggerSelection*process.GetPFInfo)
+process.p = cms.Path(process.ak4PuppiJets*process.GetPFInfo)
+
+print process.dumpPython() 

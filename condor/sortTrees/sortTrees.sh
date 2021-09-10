50af187b76a6dbb90cae4640e03cf0e1771b23bf
Fabio Iemmi <fabio.iemmi@cern.ch>
2021-09-10 15:53:38 +0200

    change a path in sortTrees.sh

--- condor/sortTrees/sortTrees.sh
+++ condor/sortTrees/sortTrees.sh
@@ -4,7 +4,7 @@ echo "Starting job on " `date` #Date/time of start of job
 echo "Running on: `uname -a`" #Condor job is running on this node
 echo "System software: `cat /etc/redhat-release`" #Operating System on that node
 source /cvmfs/cms.cern.ch/cmsset_default.sh  ## if a bash script, use .sh instead of .csh
-cd /afs/cern.ch/work/f/fiemmi/private/CMSSW_10_6_20/src/PFCandInfo/PFCandInfoAnalyzer/sorting_framework/sorted_files
+cd /afs/cern.ch/work/f/fiemmi/private/CMSSW_10_6_20/src/PFCandInfo/PFCandInfoAnalyzer/sorting_framework/sorted_files/EXT80k_v9-v1
 eval `scramv1 runtime -sh` # cmsenv is an alias not on the workers
 declare -i nfile=${1}
 root -l -q -b '/afs/cern.ch/work/f/fiemmi/private/CMSSW_10_6_20/src/PFCandInfo/PFCandInfoAnalyzer/sorting_framework/sortTrees.cpp('$nfile')'
